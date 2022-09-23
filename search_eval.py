3bb6dfcc116278ebaf4afb7f6e66f3a8fc436d32
Parth Shukla <pshukl21@illinois.edu>
2022-09-23 14:25:46 -0500

    test3

--- search_eval.py
+++ search_eval.py
@@ -11,7 +11,7 @@ def load_ranker(cfg_file):
     The parameter to this function, cfg_file, is the path to a
     configuration file used to load the index.
     """
-    return metapy.index.OkapiBM25(k1=3, b=0.7, k3 = 3.1)
+    return metapy.index.OkapiBM25(k1=10, b=0.7, k3 = 3.1)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
